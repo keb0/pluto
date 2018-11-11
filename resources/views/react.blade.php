f3dceb39f2b86433647dfded3eb6a9859cd25340
k <k@example.com>
2018-11-11 23:16:44 +0900

    react

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -4,10 +4,11 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>React</title>
+        <title>Laravel</title>
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div id="example"></div>
-        <script src="{{ asset('js/app.js')}}"></script>
+        <script src="{{asset('js/app.js')}}" ></script>
     </body>
 </html>
