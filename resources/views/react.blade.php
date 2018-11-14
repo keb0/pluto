92d9ec6561ccab27dcf6da735ce46cfb32556869
k <k@example.com>
2018-11-14 21:52:15 +0900

    react

--- resources/views/react.blade.php
+++ resources/views/react.blade.php
@@ -5,10 +5,20 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <link href="{{secure_asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        @if(app('env')=='local')
+            <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        @endif
+        @if(app('env')=='production')
+            <link href="{{secure_asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        @endif
     </head>
     <body>
         <div id="example"></div>
-        <script src="{{secure_asset('js/app.js')}}" ></script>
+        @if(app('env')=='local')
+            <script src="{{asset('js/app.js')}}" ></script>
+        @endif
+        @if(app('env')=='production')
+            <script src="{{secure_asset('js/app.js')}}" ></script>
+        @endif
     </body>
 </html>
