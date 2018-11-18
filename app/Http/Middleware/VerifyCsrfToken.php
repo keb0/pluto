ece41cff26b5800a4361bf84e84a5d96cd5d77db
k <k@example.com>
2018-11-18 21:50:12 +0900

    csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         // slack
-        'https://api.slack.com/robots',
+        // 'https://api.slack.com/robots',
     ];
 }
